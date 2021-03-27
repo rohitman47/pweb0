d55db4f00b668434f9e71c146d8eacee099eb32c
Rohit Reddy Gangwar <rohitreddy.gangwar@gmail.com>
2021-03-27 18:05:04 +0530

    Re-uploaded.

---
+++ script.js
@@ -0,0 +1,11 @@
+var color1 = document.getElementById("color1")
+var color2 = document.getElementById("color2")
+var css = document.querySelector("h3")
+var body = document.querySelector("body")
+
+function gradientColor(){
+    body.style.background = "linear-gradient(to right, "+ color1.value +", "+ color2.value +")"
+    css.textContent = body.style.background + ";";
+}
+color1.addEventListener("input", gradientColor);
+color2.addEventListener("input", gradientColor);
